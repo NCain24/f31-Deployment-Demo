c85af57f9ceaec402d9a3512b507249e8c08025a
Noah Cain <noahcain.work@gmail.com>
2022-11-23 14:04:40 -0800

    added new base url

--- public/main.js
+++ public/main.js
@@ -1,5 +1,17 @@
 const btn = document.querySelector('button')
+const btn2 = document.querySelector('#button2')
+
+const baseURL = 'http://ec2-18-237-218-13.us-west-2.compute.amazonaws.com/'
 
 const clickHandler = () => alert('The cat name is: Rascal')
 
+const getCat = () => {
+    axios.get(`${baseURL}/api/cat`)
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(err => console.log(err))
+}
+
 btn.addEventListener('click', clickHandler)
+btn2.addEventListener('click', getCat)
