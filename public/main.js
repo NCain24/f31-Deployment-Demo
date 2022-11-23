a4e5f14529e69cb366b4404425c57a1b37b8c90b
Noah Cain <noahcain.work@gmail.com>
2022-11-23 14:07:52 -0800

    update URL

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
 const btn = document.querySelector('button')
 const btn2 = document.querySelector('#button2')
 
-const baseURL = 'http://ec2-18-237-218-13.us-west-2.compute.amazonaws.com/'
-
 const clickHandler = () => alert('The cat name is: Rascal')
 
 const getCat = () => {
-    axios.get(`${baseURL}/api/cat`)
+    axios.get(`http://ec2-18-237-218-13.us-west-2.compute.amazonaws.com/api/cat`)
     .then(res => {
         alert(res.data)
     })
